023d5f136809aaacf2c9cbbc607265b51d05aff4
Preston Copeland <copelandps1@gmail.com>
2015-08-01 13:10:52 -0400

    update hours of operation

---
+++ app/helpers/gallery_helper.rb
@@ -0,0 +1,5 @@
+module GalleryHelper
+  def photos
+    %w(milqcutnedge.jpg lamontcutnedge.jpg cutnedgechair1.jpg cutnedgeshop1.jpg cutnedgechair2.jpg cutnedgeshop3.jpg cutnedgeshop4.jpg cutnedgebarber.jpg)
+  end
+end
