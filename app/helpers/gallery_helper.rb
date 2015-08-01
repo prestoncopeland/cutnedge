bc57b604f1d9fbbb09ec6edf2ebb6b9d21db12ce
Preston Copeland <copelandps1@gmail.com>
2015-08-01 13:15:37 -0400

    fix gallery

--- app/helpers/gallery_helper.rb
+++ app/helpers/gallery_helper.rb
@@ -1,5 +1,5 @@
 module GalleryHelper
   def photos
-    %w(milqcutnedge.jpg lamontcutnedge.jpg cutnedgechair1.jpg cutnedgeshop1.jpg cutnedgechair2.jpg cutnedgeshop3.jpg cutnedgeshop4.jpg cutnedgebarber.jpg)
+    %w(milqcutnedge.jpg lamontcutnedge.jpg cutnedgechair1.jpg cutnedgeshop1.jpg cutnedgechair2.jpg cutnedgeshop3.jpg cutnedgeshop4.jpg cutnedgebarber.jpg cutnedgeoutside.png)
   end
 end
